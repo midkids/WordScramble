caa7b940a8aa3677b4ffa068a0bd0a69b289211a
midkids <83714759+midkids@users.noreply.github.com>
2026-03-17 21:51:15 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Myron Snelson on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
